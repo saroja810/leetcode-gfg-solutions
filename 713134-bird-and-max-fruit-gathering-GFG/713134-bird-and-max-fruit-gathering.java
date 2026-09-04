9677f2ae3c8eef31821bc087bd1b2f9edd8a3b7a
Saroja Guttala <sarojaguttala24@gmail.com>
2026-09-04 12:23:15 +0530

    Bird and Max Fruit Gathering

---
+++ 713134-bird-and-max-fruit-gathering-GFG/713134-bird-and-max-fruit-gathering.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxFruits(ArrayList<Integer> arr, int m) {
+        // code here
+        int n =arr.size();
+        if(n==0 || m<=0)
+            return 0;
+
+        if(m>=n){
+            int tot=0;
+            for(int x:arr)
+                tot += x;
+            return tot;
+        }
+        
+        int windowsum=0;
+        for(int i=0;i<m;i++){
+            windowsum+=arr.get(i);
+        }
+        
+        int maxsum=windowsum;
+        for(int i=1;i<n;i++){
+            windowsum+=arr.get((i+m-1)%n);
+            windowsum-=arr.get(i-1);
+            maxsum=Math.max(maxsum,windowsum);
+        }
+        
+        return maxsum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
