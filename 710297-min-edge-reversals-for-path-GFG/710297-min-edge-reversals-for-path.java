5537d949ca2336999ac3d7b050a03a452f7d376b
Saroja Guttala <sarojaguttala24@gmail.com>
2026-09-17 21:27:31 +0530

    Added solution - Time: O(E log V), Space: O(V + E)

---
+++ 710297-min-edge-reversals-for-path-GFG/710297-min-edge-reversals-for-path.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minimumEdgeReversal(int[][] edges, int n, int src, int dst) {
+        // code here
+
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+
+        for(int i = 0 ; i <= n ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(new int[]{v , 0});
+            adj.get(v).add(new int[]{u , 1});
+        }
+
+        int[] dist = new int[n+1];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            return Integer.compare(a[1],b[1]);
+        });
+
+        pq.offer(new int[]{src , 0});
+
+        boolean[] visited = new boolean[n+1];
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int cost = curr[1];
+
+            visited[node] = true;
+
+            for(int[] neigh : adj.get(node)){
+                int nei = neigh[0];
+                int newCost = cost + neigh[1];
+                if(!visited[nei] && newCost < dist[nei]){
+                    dist[nei] = newCost;
+                    pq.offer(new int[]{nei , newCost});
+                }
+            }
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
