c251ee42e9de358d5a7b5bbf7f182d85f1c64d8f
Saroja Guttala <sarojaguttala24@gmail.com>
2026-09-18 12:04:51 +0530

    1621. Number of Sets of K Non-Overlapping Line Segments

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,43 @@
+class Solution {
+    private static final long MOD = 1_000_000_007L;
+
+    private long modPow(long base, long exp) {
+        long result = 1;
+
+        while (exp > 0) {
+            if ((exp & 1L) != 0) {
+                result = result * base % MOD;
+            }
+
+            base = base * base % MOD;
+
+            exp >>= 1;
+        }
+
+        return result;
+    }
+
+    public int numberOfSets(int n, int k) {
+        long N = n + k - 1L;
+        long R = 2L * k;
+
+        R = Math.min(R, N - R);
+
+        long numerator = 1;
+        long denominator = 1;
+
+        for (long i = 1; i <= R; i++) {
+            numerator = numerator * (N - R + i) % MOD;
+
+            denominator = denominator * i % MOD;
+        }
+
+        long inverseDenominator = modPow(denominator, MOD - 2);
+
+        return (int) (numerator * inverseDenominator % MOD);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
